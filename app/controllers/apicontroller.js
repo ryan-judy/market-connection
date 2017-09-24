893652e2dd15d0df6e172fe4ced80428e9fd65ef
ryan-judy <rj113909@gmail.com>
2017-09-24 15:28:59 -0400

    added accordian and chart.js

--- app/controllers/apicontroller.js
+++ app/controllers/apicontroller.js
@@ -3,7 +3,7 @@ var axios = require("axios");
 //var userAddress = req.params.address;
 
 axios.defaults.headers.common['Accept'] = "application/json";       
-axios.defaults.headers.common['apikey'] = "1e51e1c38758ccd4c0cc119e92223a3d";
+axios.defaults.headers.common['apikey'] = "add4ecd854faff59b2bfc73949fa8e36";
 
 
 module.exports = {
@@ -16,7 +16,7 @@ apiCall: function(req, res) {
     delete userAddressTrim[3];
 
 var queryURL = "https://search.onboard-apis.com/propertyapi/v1.0.0/avm/detail?address1=" + userAddressTrim[0] + "&address2=" + userAddressTrim[1] + userAddressTrim[2]+"";
-  var queryURL2 = "https://search.onboard-apis.com/propertyapi/v1.0.0/sale/snapshot?address1=" + userAddressTrim[0] + "&address2=" + userAddressTrim[1] + userAddressTrim[2]+"&radius=1";
+  var queryURL2 = "https://search.onboard-apis.com/propertyapi/v1.0.0/sale/snapshot?address1=" + userAddressTrim[0] + "&address2=" + userAddressTrim[1] + userAddressTrim[2]+"&radius=2&minsaleamt=40000";
     var queryURL3 = "https://search.onboard-apis.com/propertyapi/v1.0.0/salestrend/snapshot?geoid=ZI" + userAddressTrim[4] + "&interval=monthly&startyear=2017&endyear=2017&startmonth=january&endmonth=december";
   console.log(queryURL);
   axios.get(queryURL),
--- public/js/value.js
+++ public/js/value.js
@@ -1,13 +1,40 @@
 $(document).ready(function() {
 
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({address:$('#mapAddress').val(),region:'no'},function(results,status){
-      if(status.toLowerCase() == 'ok'){
-        var coords = new google.maps.LatLng(results[0]['geometry']['location'].lat(),results[0]['geometry']['location'].lng());
-       console.log(coords.lat());
-        console.log(coords.lng());
-      }
+var dataArr = [];
+$('.chart').each(function(index,data) {
+   var plot = $(this).val();
+    plot = parseInt(plot);
+    dataArr.push(plot);
     });
+    console.log(dataArr)
 
+      var lineChartData = {
+    labels: ["Jan", "Feb", "March", "April", "May", "June", "July"],
+    datasets: [{
+        fillColor: "rgba(220,220,220,0)",
+        strokeColor: "rgba(220,180,0,1)",
+        pointColor: "rgba(220,180,0,1)",
+        data: dataArr
+    }]
+
+}
+
+Chart.defaults.global.animationSteps = 50;
+Chart.defaults.global.tooltipYPadding = 16;
+Chart.defaults.global.tooltipCornerRadius = 0;
+Chart.defaults.global.tooltipTitleFontStyle = "normal";
+Chart.defaults.global.tooltipFillColor = "rgba(0,160,0,0.8)";
+Chart.defaults.global.animationEasing = "easeOutBounce";
+Chart.defaults.global.responsive = true;
+Chart.defaults.global.scaleLineColor = "black";
+Chart.defaults.global.scaleFontSize = 16;
+
+var ctx = document.getElementById("canvas").getContext("2d");
+var LineChartDemo = new Chart(ctx).Line(lineChartData, {
+    pointDotRadius: 10,
+    bezierCurve: false,
+    scaleShowVerticalLines: false,
+    scaleGridLineColor: "black"
+});
 
 });
